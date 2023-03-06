103e6aa08ccc9507cfd1418701f34448c20cead2
x3imal <aasurov@gmail.com>
2023-03-06 22:56:51 +0300

    1 task completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        task1();
+
+    }
+
+    public static void task1(){
+        System.out.println("Задача 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
